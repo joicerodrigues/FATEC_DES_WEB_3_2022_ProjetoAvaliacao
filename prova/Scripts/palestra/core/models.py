68942962152a8a11d19c0cf091533fd030f542cc
Joice Rodrigues <joicerodrigues020@gmail.com>
2022-11-24 17:26:41 -0300

    🚀 adicionado ORM e configurada views/urls

--- prova/Scripts/palestra/core/models.py
+++ prova/Scripts/palestra/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class PalestaModel(models.Model):
+    nome = models.CharField('Palestra', max_lenght=90)
+    dia = models.IntegerField('Data')
+    mes = models.IntegerField('Mês')
+    horario = models.IntegerField('Horário')
+    sala = models.IntegerField('Sala')
+    miniCurriculo = models.IntegerField('Mini Curriculo')
+    modificado_em = models.DateTimeField(
+        verbose_name='modificado em',
+        auto_now_add=False, auto_now=True)
+
+    def _str_(self):
+        return self.nome
